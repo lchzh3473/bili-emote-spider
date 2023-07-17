afb52e77a0bfbe794c1d614fd4fdd4bee095a409
lchzh3473 <lchzh3473@163.com>
2023-07-18 01:13:49 +0800

    commit

--- get_img.mjs
+++ get_img.mjs
@@ -1,11 +1,11 @@
 import fs from 'fs';
 import { createHash } from 'crypto';
 const infinityFetch = async (...arg) => {
-	try {
-		return await fetch(...arg);
-	} catch {
-		return await infinityFetch(...arg);
-	}
+  try {
+    return await fetch(...arg);
+  } catch {
+    return await infinityFetch(...arg);
+  }
 };
 const em_path = 'meta';
 const im_path = 'img';
@@ -15,43 +15,43 @@ const escape = str => str.replace(/[\\\/:*?"<>|]/g, a => `%${a.charCodeAt().toSt
 // for (const i of fs.readdirSync(pk_path)) fs.unlinkSync(`${pk_path}/${i}`); //delete
 const arr = await fs.promises.readdir(em_path);
 for (const e of arr) {
-	const pkg = JSON.parse(fs.readFileSync(`${em_path}/${e}`))?.packages?.[0]; //
-	if (pkg) {
-		const { id, text, url } = pkg;
-		const id_str = String(id).padStart(4, '0');
-		const url_str = url.split('@')[0];
-		const ext = url_str.split('.').pop();
-		const response = await infinityFetch(url_str);
-		const buffer = Buffer.from(await response.arrayBuffer());
-		const sha1 = createHash('sha1').update(buffer).digest('hex');
-		const name = `${id_str}.${escape(text)}.${sha1.slice(0, 8)}.${ext}`;
-		console.log(id, name);
-		if (!fs.existsSync(`${pk_path}/${name}`)) fs.writeFileSync(`${pk_path}/${name}`, buffer);
-	}
-	for (const e of pkg?.emote || []) {
-		const { id, text, url, gif_url, type } = e;
-		const id_str = String(id).padStart(5, '0');
-		if (type === 4) {
-			const buffer = url;
-			const sha1 = createHash('sha1').update(buffer).digest('hex');
-			const name = `${id_str}.${escape(text)}.${sha1.slice(0, 8)}.txt`;
-			console.log(id, name);
-			if (!fs.existsSync(`${im_path}/${name}`)) fs.writeFileSync(`${im_path}/${name}`, buffer);
-		} else {
-			const url_splited = url.split('@')[0];
-			const ext = url_splited.split('.').pop();
-			const response = await infinityFetch(url_splited);
-			const buffer = Buffer.from(await response.arrayBuffer());
-			const sha1 = createHash('sha1').update(buffer).digest('hex');
-			const name = `${id_str}.${escape(text)}.${sha1.slice(0, 8)}.${ext}`;
-			if (!fs.existsSync(`${im_path}/${name}`)) fs.writeFileSync(`${im_path}/${name}`, buffer);
-		}
-		if (gif_url) {
-			const response = await infinityFetch(gif_url);
-			const buffer = Buffer.from(await response.arrayBuffer());
-			const sha1 = createHash('sha1').update(buffer).digest('hex');
-			const name = `${id_str}.${escape(text)}.${sha1.slice(0, 8)}.gif`;
-			if (!fs.existsSync(`${im_path}/${name}`)) fs.writeFileSync(`${im_path}/${name}`, buffer);
-		}
-	}
+  const pkg = JSON.parse(fs.readFileSync(`${em_path}/${e}`))?.packages?.[0]; //
+  if (pkg) {
+    const { id, text, url } = pkg;
+    const id_str = String(id).padStart(4, '0');
+    const url_str = url.split('@')[0];
+    const ext = url_str.split('.').pop();
+    const response = await infinityFetch(url_str);
+    const buffer = Buffer.from(await response.arrayBuffer());
+    const sha1 = createHash('sha1').update(buffer).digest('hex');
+    const name = `${id_str}.${escape(text)}.${sha1.slice(0, 8)}.${ext}`;
+    console.log(id, name);
+    if (!fs.existsSync(`${pk_path}/${name}`)) fs.writeFileSync(`${pk_path}/${name}`, buffer);
+  }
+  for (const e of pkg?.emote || []) {
+    const { id, text, url, gif_url, type } = e;
+    const id_str = String(id).padStart(5, '0');
+    if (type === 4) {
+      const buffer = url;
+      const sha1 = createHash('sha1').update(buffer).digest('hex');
+      const name = `${id_str}.${escape(text)}.${sha1.slice(0, 8)}.txt`;
+      console.log(id, name);
+      if (!fs.existsSync(`${im_path}/${name}`)) fs.writeFileSync(`${im_path}/${name}`, buffer);
+    } else {
+      const url_splited = url.split('@')[0];
+      const ext = url_splited.split('.').pop();
+      const response = await infinityFetch(url_splited);
+      const buffer = Buffer.from(await response.arrayBuffer());
+      const sha1 = createHash('sha1').update(buffer).digest('hex');
+      const name = `${id_str}.${escape(text)}.${sha1.slice(0, 8)}.${ext}`;
+      if (!fs.existsSync(`${im_path}/${name}`)) fs.writeFileSync(`${im_path}/${name}`, buffer);
+    }
+    if (gif_url) {
+      const response = await infinityFetch(gif_url);
+      const buffer = Buffer.from(await response.arrayBuffer());
+      const sha1 = createHash('sha1').update(buffer).digest('hex');
+      const name = `${id_str}.${escape(text)}.${sha1.slice(0, 8)}.gif`;
+      if (!fs.existsSync(`${im_path}/${name}`)) fs.writeFileSync(`${im_path}/${name}`, buffer);
+    }
+  }
 }
--- get_pkg.mjs
+++ get_pkg.mjs
@@ -1,23 +1,23 @@
 import fs from 'fs';
 const infinityFetch = async (...arg) => {
-	try {
-		return await fetch(...arg);
-	} catch {
-		return await infinityFetch(...arg);
-	}
+  try {
+    return await fetch(...arg);
+  } catch {
+    return await infinityFetch(...arg);
+  }
 };
 const em_path = 'meta';
 async function getEmote(id) {
-	const response = await infinityFetch(`https://api.bilibili.com/x/emote/package?business=reply&ids=${id}`);
-	const { data } = await response.json();
-	return data;
+  const response = await infinityFetch(`https://api.bilibili.com/x/emote/package?business=reply&ids=${id}`);
+  const { data } = await response.json();
+  return data;
 }
 for (let i = 0; i < 3000; i++) {
-	console.log(i);
-	await new Promise(resolve => setTimeout(resolve, 500)); //sleep 0.5s
-	const data = await getEmote(i);
-	if (!data?.packages) continue;
-	fs.writeFileSync(`${em_path}/${i}.json`, JSON.stringify(await getEmote(i), null, '\t'));
+  console.log(i);
+  await new Promise(resolve => setTimeout(resolve, 500)); //sleep 0.5s
+  const data = await getEmote(i);
+  if (!data?.packages) continue;
+  fs.writeFileSync(`${em_path}/${i}.json`, JSON.stringify(await getEmote(i), null, '\t'));
 }
 // console.log(await getEmote(1));
 // fs.writeFileSync('qwq.json', JSON.stringify(arr, null, '\t'));
